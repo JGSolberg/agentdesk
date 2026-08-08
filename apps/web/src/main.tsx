295a0354bf0f8f40ed0a93ff555681c32bf55228
Jason Solberg <JGSolberg@gmail.com>
2026-08-07 21:58:05 -0500

    Mount command palette globally

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import CommandPalette from "./CommandPalette";
 import "./styles.css";
 import "./board.css";
 import "./ticket-detail.css";
 import "./ticket-lifecycle.css";
 import "./repositories.css";
+import "./command-palette.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <CommandPalette />
     </BrowserRouter>
   </React.StrictMode>,
 );
